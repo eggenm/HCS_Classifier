a8edab61a44e83b51341f7a5e47127886e019746
M E <eggenm@GMAIL.COM>
2020-01-09 10:01:15 -0800

    moving classification code to the server

--- simple_classifiers.py
+++ simple_classifiers.py
@@ -30,14 +30,14 @@ import matplotlib.pyplot as plt
 # =============================================================================
 base_dir = dirfuncs.guess_data_dir()
 
-concessions = ['gar_pgm', 'app_kalbar', 'crgl_stal']
-classConcession = 'app_kaltim'
+concessions = ['gar_pgm','app_kaltim' , 'app_kalbar']
+classConcession = 'crgl_stal'
 bands = ['bands_base', 'bands_radar', 'evi2_only']
 sample_rate=0.010
 pixel_window_size = 1
 iterations = 1
 doGridSearch = True
-scheme='ALL'
+scheme='3class'
 suffix = 'RF_x' + str(iterations) + '_'+scheme + '_'+ str(int(round(sample_rate*100, 0))) +'_BaseRadarEVI.tif'
 #classes = {1: "HCSA",
      #      0: "NA"}
@@ -133,7 +133,7 @@ for seed in range(1,iterations+1):
     # # =============================================================================
 
 
-    clf = rfc(n_estimators=400, max_depth = 8, max_features = .75, max_leaf_nodes = 15,
+    clf = rfc(n_estimators=400, max_depth = 12, max_features = .75, max_leaf_nodes = 15,
               #random_state=seed,
               random_state=16,
               oob_score = True, n_jobs = -1,
