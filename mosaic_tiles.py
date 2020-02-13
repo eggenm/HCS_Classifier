da85d972ebf91713d06585596aa1ef64abb6111c
M E <eggenm@GMAIL.COM>
2020-02-13 07:21:42 -0800

    pushing to aurora to see if i can run there

--- mosaic_tiles.py
+++ mosaic_tiles.py
@@ -8,7 +8,7 @@ dirpath = r'/home/eggen/data/concession/Sumatra/in/2015'
 out_fp = r'/home/eggen/data/concession/Sumatra/out/2015/'
 
 my_dict = sat_ops.l8_band_dict.copy()
-#my_dict.update(sat_ops.s1_band_dict)
+my_dict.update(sat_ops.s1_band_dict)
 for band in my_dict.values():
     search_criteria='*'+band+'.tif'
     out_file = band+'.tif'
