184552ee9a9c108fed379294671ce3256c2e667a
M E <eggenm@GMAIL.COM>
2020-05-05 13:18:04 -0700

    ingest dem

--- mosaic_tiles.py
+++ mosaic_tiles.py
@@ -4,12 +4,13 @@ import satellite_image_operations as sat_ops
 import glob
 import os
 
-dirpath = r'/home/eggen/data/concession/Kalimantan/in/2015'
-out_fp = r'/home/eggen/data/concession/Kalimantan/out/2015/'
+dirpath = r'/home/eggen/data/concession/Sumatra/in/2015'
+out_fp = r'/home/eggen/data/concession/Sumatra/out/2015/'
 
-#my_dict = sat_ops.l8_band_dict.copy()
-#my_dict.update(sat_ops.s1_band_dict)
-my_dict=sat_ops.dem_band_dict.copy()
+my_dict = sat_ops.l8_band_dict.copy()
+my_dict.update(sat_ops.s1_band_dict)
+my_dict.update(sat_ops.dem_band_dict)
+print('BANDS:  ',my_dict.values())
 for band in my_dict.values():
     search_criteria='*'+band+'.tif'
     out_file = band+'.tif'
@@ -18,13 +19,13 @@ for band in my_dict.values():
     print(q)
     image_files = glob.glob(q)
     out = os.path.join(out_fp, out_file)
-    print(image_files)
+    #print(image_files)
     src_files_to_mosaic = []
     for f in image_files:
         src = rasterio.open(f)
         src_files_to_mosaic.append(src)
 
-    print(src_files_to_mosaic)
+  #  print(src_files_to_mosaic)
     mosaic, out_trans = merge(src_files_to_mosaic)
     out_meta = src.meta.copy()
     # Update the metadata
