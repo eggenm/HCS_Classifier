d0403a35370d94d024af626c33fd9d3b021764fd
M E <eggenm@GMAIL.COM>
2020-09-08 12:25:22 -0700

    papua ingest

--- ingest_by_grid.py
+++ ingest_by_grid.py
@@ -30,11 +30,11 @@ lat_edge = .5
 #site = 'Kalimantan'
 years = [#2015,
         # 2016,
-    # 2017,
-    2018,
+     2017,
+   # 2018,
   #  2019
          ]
-start = 1
+start = 647
 #years= [2017,2018,2019]
 site = 'None'
 out_path = dirfuncs.guess_data_dir()
