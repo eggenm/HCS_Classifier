3f9513d13242638fb911fb8b3d1133fee7e10d78
M E <eggenm@GMAIL.COM>
2020-09-16 15:15:04 -0700

    process kali

--- mosaic_tiles.py
+++ mosaic_tiles.py
@@ -9,8 +9,8 @@ import os
 #my_dict = sat_ops.l8_band_dict.copy()
 #my_dict = sat_ops.soil_band_dict.copy()
 #my_dict = sat_ops.dem_band_dict.copy()
-my_dict=sat_ops.s1_band_dict.copy()
-my_dict.update(sat_ops.s2_band_dict)
+my_dict=sat_ops.s2_band_dict.copy()
+#my_dict.update(sat_ops.s2_band_dict)
 #my_dict.update(sat_ops.dem_band_dict)
 print('BANDS:  ',my_dict.values())
 island='Kalimantan'
