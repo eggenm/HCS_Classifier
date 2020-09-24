5476f855c1b5b4e1517f3df343dc5301e3534e8e
M E <eggenm@GMAIL.COM>
2020-09-24 13:46:43 -0700

    supplemtary class ingest

--- ShapefileHelper.py
+++ ShapefileHelper.py
@@ -24,7 +24,6 @@ import satellite_image_operations as sat_ops
 
 image_cache = imagery_data.Imagery_Cache.getInstance()
 
-#out_tif = r'C:\\Users\\ME\\Dropbox\\HCSproject\\data\\PoC\\Sumatra\\out\\2015\\jambi_crop_blue_max.tif'
 input='C:\\Users\\ME\\Dropbox\\HCSproject\\data\\app_files\\stratified_shapefiles\\Jambi_WKS_Stratification.shp'
 
 supplemental_class_codes = {
@@ -47,6 +46,7 @@ def ingest_kml_fixed_classes():
     imageSumatra = image_cache.get_band_by_context_year('nir_max', 'Sumatra', 2019)
     imageKalimantan = image_cache.get_band_by_context_year('nir_max', 'Kalimantan', 2019)
     imagePapua = image_cache.get_band_by_context_year('nir_max', 'Papua', 2019)
+
     for landcover in supplemental_class_codes.keys():
         print(landcover)
         input = os.path.join(dirfuncs.guess_data_dir(), 'supplementary_class', landcover,'doc.kml')
@@ -73,9 +73,11 @@ def ingest_kml_fixed_classes():
         bands = my_dict.values()
         print(bands)
         #bands = ['nir_max']
-        shapes = ((shape(feature["geometry"]).buffer(0), (feature['properties']['Description']),
-                   feature['properties']['Name']) for feature in features)
+
+
         for band in bands:
+            shapes = ((shape(feature["geometry"]).buffer(0), (feature['properties']['Description']),
+                       feature['properties']['Name']) for feature in features)
             for geom in shapes:
                 print(geom)
 
@@ -90,18 +92,17 @@ def ingest_kml_fixed_classes():
                 geo = gpd.GeoDataFrame({'geometry': bbox}, index=[0], crs=from_epsg(4326))
                 geo = geo.to_crs(crs=from_epsg(4326))
                 coords = getFeatures(geo)
-                try:
+                if name.index('kal')>-1:
                     out_img = imageKalimantan.rio.clip(coords, imageKalimantan.rio.crs)
                     island = 'Kalimantan'
-                except:
-                    try:
-                        print("CLIP ERROR trying other island")
+                elif name.index('sum')>-1:
                         out_img = imageSumatra.rio.clip(coords, imageSumatra.rio.crs)
                         island = 'Sumatra'
-                    except:
-                        print("CLIP ERROR trying other island")
+                elif name.index('pap') > -1:
                         out_img = imagePapua.rio.clip(coords, imagePapua.rio.crs)
                         island = 'Papua'
+                else:
+                    raise RuntimeError
                 # meta = out_img.meta.copy()
                 print(island)
                 trans = out_img.transform
@@ -111,15 +112,14 @@ def ingest_kml_fixed_classes():
                 dtype = rio.int16
                 # burned = rioft.rasterize(shapes=geom, fill=0)
                 out_fn = os.path.join(dirfuncs.guess_data_dir(),'supplementary_class', landcover, name+'.tif')
-                # with rio.open(out_fn, 'w+', driver='GTiff',
-                #               height=height, width=width,
-                #               crs=crs, dtype=dtype, transform=trans, count=1) as out:
-                #     out_arr = out.read(1)
-                #     burned = rioft.rasterize(shapes=[(geom[0], supplemental_class_codes[landcover])], fill=-9999, out=out_arr, transform=out.transform)
-                #     burned = np.where(burned != supplemental_class_codes[landcover], -9999, burned) #NoData the other pixels
-                #     out.write_band(1, burned)
-                # out.close()
-
+                with rio.open(out_fn, 'w+', driver='GTiff',
+                              height=height, width=width,
+                              crs=crs, dtype=dtype, transform=trans, count=1) as out:
+                    out_arr = out.read(1)
+                    burned = rioft.rasterize(shapes=[(geom[0], supplemental_class_codes[landcover])], fill=-9999, out=out_arr, transform=out.transform)
+                    burned = np.where(burned != supplemental_class_codes[landcover], -9999, burned) #NoData the other pixels
+                    out.write_band(1, burned)
+                out.close()
                 image = image_cache.get_band_by_context_year(band, island, my_year)
                 out_img = image.rio.clip(coords, image.rio.crs)
                 if out_img.dtype == 'float64':
