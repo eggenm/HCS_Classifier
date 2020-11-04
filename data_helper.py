ce84aa13375d31ca931d390599e986043cf6ce65
M E <eggenm@GMAIL.COM>
2020-11-03 20:57:54 -0800

    trying to figure out why 2019 data is weird for Kali

--- data_helper.py
+++ data_helper.py
@@ -518,7 +518,8 @@ if __name__ == "__main__":
     #write_input_data=True
     for band in bands:
         x = get_input_data([band], str(2019), ['Kalimantan'], True)
-    
+        x = False
+
    # x = get_input_data(bands, str(2018), ['Kalimantan'], True)
   #  x = get_input_data([ 'elevation'],  str(2015), ['gar_pgm', 'Bumitama_PTGemilangMakmurSubur','PTAgroAndalan','PTMitraNusaSarana', 'Bumitama_PTDamaiAgroSejahtera']
    #                    , False )#,
